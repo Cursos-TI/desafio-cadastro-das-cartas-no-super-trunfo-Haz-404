1e0c5122cd059097f6f71269f09f4fe0abe74aa6
Haz-404 <lipebarbosa2405@gmail.com>
2025-03-18 20:08:52 +0000

    2 cartas prontas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-
-char estado1;
-char codigo1[20];
-char nome1[20];
-int populacao1;
-float area1; 
-float pib1;
-int pt1;
+//variaveis das cartas
+char estado1, estado2;
+char codigo1, codigo2[20];
+char nome1, nome2[20];
+int populacao1, populacao2;
+float area1, area2; 
+float pib1, pib2;
+int pt1, pt2;
 // cadastro das informacoes
 printf("digite o estado: ");
 scanf("%c", &estado1);
@@ -32,9 +32,51 @@ printf("população:%d\n", populacao1);
 printf("area:%.2f\n", area1);
 printf("PIB:%.2f\n", pib1);
 printf("pontos turisticos:%d\n", pt1);
+
+//carta 2
+//cadastro das informaçoes
+
+printf("digite o estado: ");
+scanf("%c", &estado2);
+printf("digite o codigo:");
+scanf("%s", &codigo2);
+printf("digite o nome:");
+scanf("%s", &nome2);
+printf("digite a populacao:");
+scanf("%d", &populacao2);
+printf("digite a area:");
+scanf("%f", &area2);
+printf("digite o pib:");
+scanf("%f", &pib2);
+printf("digite os pontos turisticos:");
+scanf("%d", &pt2);
+
+//resultados da carta 2
+
+printf("estado:%c\n", estado2);
+printf("codigo:%s\n", codigo2);
+printf("nome da cidade:%s\n", nome2);
+printf("população:%d\n", populacao2);
+printf("area:%.2f\n", area2);
+printf("PIB:%.2f\n", pib2);
+printf("pontos turisticos:%d\n", pt2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
-
-
-
-
